733d783761d973ae1beddbb40e502e3ceff25dd1
markFLA <markfrancisalbos26@gmail.com>
2026-04-10 22:02:43 +0800

    home testing

--- pages/home.php
+++ pages/home.php
@@ -1,3 +1,7 @@
 <?php
 echo "Welcome to CITAS Internship System!";
+$userid = $_SESSION["user_id"];
+$name = $_SESSION["name"];
+$role = $_SESSION["role"];
+echo "Hello, $name! You are logged in as $role.";
 ?>
