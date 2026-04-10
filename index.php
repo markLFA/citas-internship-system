3918f99b967c51aa9b8a56c86ad929470d2bcc3f
markFLA <markfrancisalbos26@gmail.com>
2026-04-10 21:35:07 +0800

    fixwd error log printing

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('log_errors', 1);
-ini_set('error_log', 'error_log.txt');
-error_reporting(E_ALL);
+
 echo "Welcome to CITAS Internship System!";
 session_start();
 require 'config/db.php';
