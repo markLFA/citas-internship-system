ffa5464f366e5eac32b5947f36ca36fc1c954fd3
mark <markfrancisalbos26@gmail.com>
2026-04-30 02:22:08 +0800

    Update db.php

--- config/db.php
+++ config/db.php
@@ -3,17 +3,27 @@ ini_set('display_errors', 1);
 ini_set('log_errors', 1);
 ini_set('error_log', 'error_log.txt');
 error_reporting(E_ALL);
-?>
-<?php
 
+function getDB() {
+  static $pdo = null;
+
+  if ($pdo === null) {
 $host = "fdb1034.awardspace.net";
 $user = "4753482_capstone";
 $pass = "787898;"; 
 $db   = "4753482_capstone"; 
 
-$conn = new mysqli($host, $user, $pass, $db);
+    $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    $options = [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // throw errors
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // default fetch mode
+      PDO::ATTR_EMULATE_PREPARES => false, // use real prepared statements
+    ];
+
+    $pdo = new PDO($dsn, $user, $pass, $options);
+  }
+
+  return $pdo;
 }
 ?>
