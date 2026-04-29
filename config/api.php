18c54962a13d47804ef4f2b170cb71014d14d680
mark <markfrancisalbos26@gmail.com>
2026-04-30 02:46:35 +0800

    Update api.php

--- config/api.php
+++ config/api.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('log_errors', 1);
+ini_set('error_log', 'error_log.txt');
+error_reporting(E_ALL);
 session_start();
 header('Content-Type: application/json');
 
