1856606d3e609ea84199217d2a6d30af5dac19dd
mark <markfrancisalbos26@gmail.com>
2026-04-30 02:13:48 +0800

    Update functions.php added getInternProfile function

--- config/functions.php
+++ config/functions.php
@@ -7,4 +7,24 @@ function getUsers() {
   $userId = $user['id'];
   $stmt = $conn->query("SELECT id, username FROM intern_profiles");
   return $stmt->fetchAll(PDO::FETCH_ASSOC);
+  
+}
+function getInternProfile() {
+  // make sure session is started BEFORE calling this function
+  if (!isset($_SESSION['user']['id'])) {
+    return null; // or throw error if you prefer
+  }
+
+  $pdo = getDB(); // assuming you’re using the getDB() pattern
+
+  $stmt = $pdo->prepare("
+    SELECT *
+    FROM intern_profiles
+    WHERE user_id = ?
+    LIMIT 1
+  ");
+
+  $stmt->execute([$_SESSION['user']['id']]);
+
+  return $stmt->fetch(PDO::FETCH_ASSOC);
 }
