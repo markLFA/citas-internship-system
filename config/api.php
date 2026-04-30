0194e697d82037acb46441137c2613615615792f
markFLA <markfrancisalbos26@gmail.com>
2026-04-30 16:28:08 +0800

    Fix fetch path in intern.html and add a newline in api.php

--- config/api.php
+++ config/api.php
@@ -3,6 +3,7 @@ ini_set('display_errors', 1);
 ini_set('log_errors', 1);
 ini_set('error_log', 'error_log.txt');
 error_reporting(E_ALL);
+
 session_start();
 header('Content-Type: application/json');
 
