5e0ce1931e69d66cca76e0fb73a7a7d7f2d09f84
markFLA <markfrancisalbos26@gmail.com>
2026-09-07 14:23:09 +0800

    remove school year filter from getInternsBySchoolYear function

--- config/functions.php
+++ config/functions.php
@@ -2642,7 +2642,6 @@ function getInternsBySchoolYear(string $schoolYear = ''): array
             LEFT JOIN companies  c   ON c.id         = i.company_id
             WHERE u.role     = 'intern'
               AND u.is_active = 1
-              AND i.school_year = :school_year
               AND i.id = (
                   SELECT id FROM internships
                   WHERE intern_id = u.id
