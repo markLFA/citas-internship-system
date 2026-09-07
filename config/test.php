7861773c0b7129097a772326158bead81b8eca47
markFLA <markfrancisalbos26@gmail.com>
2026-09-07 14:18:39 +0800

    refactor test.php; add school year management and intern retrieval logic

--- config/test.php
+++ config/test.php
@@ -1,6 +1,8 @@
 <?php
 
 require 'supabase.php';
+require_once __DIR__ . '/api.php';
+
 
 if (isset($_GET['test_supabase'])) {
     header('Content-Type: application/json');
@@ -8,3 +10,9 @@ if (isset($_GET['test_supabase'])) {
     echo json_encode(testSupabaseConnection(), JSON_PRETTY_PRINT);
     exit;
 }
+$schoolYear = '2026-2027';
+$interns    = getInternsBySchoolYear($schoolYear);
+echo json_encode([
+    'success' => true,
+    'interns' => $interns
+], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
